7b47575ee6fbaafbab13a4694cd9149fb51f485c
danielbd <danbenitez448@gmail.com>
2022-10-04 14:49:54 -0500

    Agregar proyecto

---
+++ src/main/java/com/ventas/ventas/service/ClienteService.java
@@ -0,0 +1,42 @@
+package com.ventas.ventas.service;
+
+
+import com.ventas.ventas.model.Cliente;
+import com.ventas.ventas.repository.IClienteRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService implements IClienteService {
+
+    @Autowired
+    private IClienteRespository clienteRepo;
+
+    @Override
+    public List<Cliente> findAll() {
+        return clienteRepo.findAll();
+    }
+
+    @Override
+    public Optional<Cliente> findById(Integer id) {
+        return clienteRepo.findById(id);
+    }
+
+    @Override
+    public Cliente create(Cliente cliente) {
+        return clienteRepo.save(cliente);
+    }
+
+    @Override
+    public Cliente update(Cliente cliente) {
+        return clienteRepo.save(cliente);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        clienteRepo.deleteById(id);
+    }
+}
